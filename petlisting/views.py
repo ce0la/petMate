ef153d1f3aaced804e9cc46e120c56d566d75f00
Olaniyi Oshunbote <oshunboteo@gmail.com>
2019-12-21 06:41:51 +0100

    updated tracked files

--- petlisting/views.py
+++ petlisting/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'petlisting/index.html')
+
+def about(request):
+    return render(request, 'petlisting/about.html')
+
+def contact(request):
+    return render(request, 'petlisting/contact.html')
--- petmate/urls.py
+++ petmate/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+import petlisting.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', petlisting.views.index, name='index'),
+    path('', petlisting.views.about, name='about'),
+    path('', petlisting.views.contact, name='contact')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
